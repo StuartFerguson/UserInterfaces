3b9b28f4ed02442418414f055600307f70ff2571
StuartFerguson <stuart_ferguson1@btinternet.com>
2019-07-19 18:31:27 +0100

    Ready for PR

--- GolfClubAdminWebSite/GolfClubAdminWebSite.IntegrationTests/Steps/UserLoginSteps.cs
+++ GolfClubAdminWebSite/GolfClubAdminWebSite.IntegrationTests/Steps/UserLoginSteps.cs
@@ -117,5 +117,109 @@ namespace GolfClubAdminWebSite.IntegrationTests.Steps
         {
             this.BrowserSession.Title.ShouldBe("Dashboard");
         }
+
+        [When(@"I click on the Manage Golf Club sidebar option")]
+        public void WhenIClickOnTheManageGolfClubSidebarOption()
+        {
+            this.BrowserSession.ClickLink("Golf Club Details");
+        }
+
+        [Then(@"I am presented with the Create Golf Club Screen")]
+        public void ThenIAmPresentedWithTheCreateGolfClubScreen()
+        {
+            this.BrowserSession.Title.ShouldBe("Create Golf Club");
+        }
+
+        [When(@"I use the following details to create a new golf club")]
+        public void WhenIUseTheFollowingDetailsToCreateANewGolfClub(Table table)
+        {
+            var tableRow = table.Rows.First();
+
+            this.BrowserSession.FillIn("Name").With(tableRow["GolfClubName"]);
+            this.BrowserSession.FillIn("AddressLine1").With(tableRow["AddressLine1"]);
+            this.BrowserSession.FillIn("AddressLine2").With(tableRow["AddressLine2"]);
+            this.BrowserSession.FillIn("Town").With(tableRow["TownCity"]);
+            this.BrowserSession.FindId("Region").FillInWith(tableRow["Region"]);
+            this.BrowserSession.FindId("PostalCode").FillInWith(tableRow["PostCode"]);
+            this.BrowserSession.FindId("TelephoneNumber").FillInWith(tableRow["TelephoneNumber"]);
+            this.BrowserSession.FindId("EmailAddress").FillInWith(tableRow["EmailAddress"]);
+            this.BrowserSession.FindId("Website").FillInWith(tableRow["Website"]);
+        }
+
+        [When(@"I click the Create Club button")]
+        public void WhenIClickTheCreateClubButton()
+        {
+            this.BrowserSession.ClickButton("Create Club");
+        }
+
+        [Then(@"I should be presented with the Edit Golf Club screen")]
+        public void ThenIShouldBePresentedWithTheEditGolfClubScreen()
+        {
+            this.BrowserSession.Title.ShouldBe("Edit Golf Club");
+        }
+
+        [When(@"I click on the Users sidebar option")]
+        public void WhenIClickOnTheUsersSidebarOption()
+        {
+            this.BrowserSession.ClickLink("Users");
+        }
+
+        [Then(@"I am presented with the list of golf club users")]
+        public void ThenIAmPresentedWithTheListOfGolfClubUsers()
+        {
+            this.BrowserSession.Title.ShouldBe("Users");
+        }
+
+        [Then(@"a user with the name ""(.*)"" should be in the list")]
+        public void ThenAUserWithTheNameShouldBeInTheList(String username)
+        {
+            Boolean hasContent = this.BrowserSession.HasContent(username, new Options
+                                                                          {
+                                                                              Timeout = TimeSpan.FromSeconds(30),
+                                                                              RetryInterval = TimeSpan.FromSeconds(1)
+                                                                          });
+            hasContent.ShouldBeTrue();
+        }
+
+        [When(@"I click on the New Match Secretary Button")]
+        public void WhenIClickOnTheNewMatchSecretaryButton()
+        {
+            this.BrowserSession.ClickLink("New Match Secretary");
+        }
+
+        [When(@"I use the following details to create a match secretary")]
+        public void WhenIUseTheFollowingDetailsToCreateAMatchSecretary(Table table)
+        {
+            TableRow tableRow = table.Rows.First();
+
+            this.BrowserSession.FillIn("GivenName").With(tableRow["FirstName"]);
+            this.BrowserSession.FillIn("FamilyName").With(tableRow["LastName"]);
+            this.BrowserSession.FindId("TelephoneNumber").FillInWith(tableRow["TelephoneNumber"]);
+            this.BrowserSession.FindId("Email").FillInWith(tableRow["Email"]);
+        }
+
+        [When(@"I click on the create user button")]
+        public void WhenIClickOnTheCreateUserButton()
+        {
+            ElementScope section = this.BrowserSession.FindButton("Create User");
+            section.Click();
+        }
+
+        [Then(@"I have logged out of the application")]
+        public void ThenIHaveLoggedOutOfTheApplication()
+        {
+            //userDropdown
+            //logout
+            ElementScope userDropdown = this.BrowserSession.FindId("userDropdown");
+            userDropdown.Click();
+
+            ElementScope logout = this.BrowserSession.FindId("logout");
+            logout.Click();
+
+            ElementScope logoutButton = this.BrowserSession.FindButton("Logout");
+            logoutButton.Click();
+        }
+
+
     }
 }
---
+++ GolfClubAdminWebSite/GolfClubAdminWebSite/Areas/MatchSecretary/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿namespace GolfClubAdminWebSite.Areas.MatchSecretary.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Services;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    [Area("MatchSecretary")]
+    public class HomeController : Controller
+    {
+        #region Fields
+
+        /// <summary>
+        /// The API client
+        /// </summary>
+        private readonly IClient ApiClient;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MatchSecretary.Controllers.HomeController" /> class.
+        /// </summary>
+        /// <param name="apiClient">The API client.</param>
+        public HomeController(IClient apiClient)
+        {
+            this.ApiClient = apiClient;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        #endregion
+    }
+}
